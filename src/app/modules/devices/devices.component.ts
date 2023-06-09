e2a085466c970e71aab454d21c2c850f7138dc89
Iaresko Vlad <vladyaresko01@gmail.com>
2023-06-09 14:22:46 +0300

    documentation

--- src/app/modules/devices/devices.component.ts
+++ src/app/modules/devices/devices.component.ts
@@ -12,22 +12,37 @@ export class DevicesComponent implements OnInit {
   pages = 1;
   devises: Device[] = [];
   constructor(private state: StateService) {
+    // Initialize the devices array with pagination and calculate the total number of pages
     this.devises = state.getAllWithPagination(this.page, this.elPerPage);
     this.pages = state.getPages(this.elPerPage);
   }
 
   ngOnInit(): void {}
 
+  /**
+   * Generates an array representing the available pages for pagination.
+   * @returns {number[]} - The array of page numbers.
+   */
   getPagesArray() {
     return Array.from({ length: this.pages }, (_, index) => index + 1);
   }
 
+  /**
+   * Sets the current page for pagination.
+   * @param {number} page - The page number to set.
+   */
   setPage(page: number) {
     this.page = page;
     this.devises = this.state.getAllWithPagination(this.page, this.elPerPage);
   }
+
+  /**
+   * Scrolls to the previous or next page based on the specified number.
+   * @param {number} num - The number to scroll the page by (-1 for previous, 1 for next).
+   */
   scrollPage(num: number) {
     this.page += num;
+    // Ensure the page is within valid bounds
     if (this.page < 1) {
       this.page = 1;
     }
@@ -38,6 +53,10 @@ export class DevicesComponent implements OnInit {
     this.devises = this.state.getAllWithPagination(this.page, this.elPerPage);
   }
 
+  /**
+   * Handles the event when the number of elements per page changes.
+   * @param {any} event - The event object.
+   */
   onAmmountChange(event: any) {
     this.elPerPage = +event.target.value;
     this.devises = this.state.getAllWithPagination(this.page, this.elPerPage);
